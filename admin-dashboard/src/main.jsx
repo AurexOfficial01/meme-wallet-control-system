4e9cbfbcb133a01bb545f378e2ae980168de02ce
AurexOfficial01 <prom91193@gmail.com>
2026-01-21 23:20:12 +0530

    Update main.jsx

--- admin-dashboard/src/main.jsx
+++ admin-dashboard/src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 import App from "./pages/App.jsx";
 import Wallets from "./pages/Wallets.jsx";
 import Transactions from "./pages/Transactions.jsx";
 import Purchases from "./pages/Purchases.jsx";
+
+axios.defaults.headers.common['x-admin-key'] = import.meta.env.VITE_ADMIN_KEY;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
