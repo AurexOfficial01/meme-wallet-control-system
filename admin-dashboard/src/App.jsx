1d1b04e230ddb12cd21d540ac2fe61234b705a94
AurexOfficial01 <prom91193@gmail.com>
2026-01-24 01:19:22 +0530

    Update App.jsx

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,37 +1,44 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import Topbar from "./components/Topbar.jsx";
 
 import Dashboard from "./pages/Dashboard.jsx";
+import Wallets from "./pages/Wallets.jsx";
 import Orders from "./pages/Orders.jsx";
 import Purchases from "./pages/Purchases.jsx";
 import Transactions from "./pages/Transactions.jsx";
-import Wallets from "./pages/Wallets.jsx";
-import SendRequest from "./pages/SendRequest.jsx";
 import AllRequests from "./pages/AllRequests.jsx";
+import SendRequest from "./pages/SendRequest.jsx";
 
-export default function App() {
+import "./styles.css";
+
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="layout">
         <Sidebar />
+
         <div className="content">
           <Topbar />
           <div className="page-container">
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/wallets" element={<Wallets />} />
               <Route path="/orders" element={<Orders />} />
               <Route path="/purchases" element={<Purchases />} />
               <Route path="/transactions" element={<Transactions />} />
-              <Route path="/wallets" element={<Wallets />} />
+
+              {/* ✅ NEW PAGES */}
+              <Route path="/all-requests" element={<AllRequests />} />
               <Route path="/send-request" element={<SendRequest />} />
-              <Route path="/requests" element={<AllRequests />} />
+
             </Routes>
           </div>
         </div>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
