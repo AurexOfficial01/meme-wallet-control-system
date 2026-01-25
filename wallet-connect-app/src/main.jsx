8416d15e20b1cbdd9b824a14db42980c39c10e4a
AurexOfficial01 <prom91193@gmail.com>
2026-01-26 00:59:03 +0530

    Update main.jsx

--- wallet-connect-app/src/main.jsx
+++ wallet-connect-app/src/main.jsx
@@ -1,15 +1,8 @@
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import App from './App.jsx';
-import BuyUsdt from './pages/BuyUsdt.jsx'; // ✅ FIXED PATH & CASE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/buy-usdt" element={<BuyUsdt />} /> {/* ✅ FIXED CASE */}
-    </Routes>
-  </BrowserRouter>
+  <App />
 );
