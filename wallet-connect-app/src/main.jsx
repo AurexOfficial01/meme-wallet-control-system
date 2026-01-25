ab7e73de829eada57a16e7ad24d6f9395919c9a7
AurexOfficial01 <prom91193@gmail.com>
2026-01-26 01:08:44 +0530

    Update main.jsx

--- wallet-connect-app/src/main.jsx
+++ wallet-connect-app/src/main.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { WalletProvider } from './context/WalletContext.js';
-
 import Home from './pages/Home.jsx';
 import BuyUsdt from './pages/BuyUsdt.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <WalletProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/buy-usdt" element={<BuyUsdt />} />
-      </Routes>
-    </BrowserRouter>
-  </WalletProvider>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <WalletProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/buy-usdt" element={<BuyUsdt />} />
+        </Routes>
+      </BrowserRouter>
+    </WalletProvider>
+  </React.StrictMode>
 );
